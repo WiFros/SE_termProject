dd207a2c1033cf2a12ece52aa044835686116ca6
WiFros <bjy123bjy@gmail.com>
2022-05-29 16:41:57 +0900

    BulidAPK
    테스트용 앱 파일 생성

---
+++ ShareOurSchedule/app/src/test/java/com/LEGENO/ShareOurSchedule/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package com.LEGENO.ShareOurSchedule;
+
+
+import org.junit.Test;
+
+class EmailValidatorTest {
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+    }
+}
+    
